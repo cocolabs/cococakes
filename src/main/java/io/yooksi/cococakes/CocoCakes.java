e3f0f39cbed51a17137f802b0e95a1757e89d9b1
Matthew Cain <yooksi@zoho.com>
2020-04-19 21:09:18 +0200

    Add main mod entry class

---
+++ src/main/java/io/yooksi/cococakes/CocoCakes.java
@@ -0,0 +1,27 @@
+package io.yooksi.cococakes;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("cococakes")
+public class CocoCakes
+{
+	public static final String MODID = "cococakes";
+	static final Logger LOGGER = LogManager.getLogger();
+
+	public CocoCakes() {
+		// Register the setup method for modloading
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		// Register ourselves for server and other game events we are interested in
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) {
+		LOGGER.info("Pre-initialization phase");
+	}
+}
