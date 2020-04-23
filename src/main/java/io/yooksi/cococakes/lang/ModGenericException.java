8931c180a11a44128d3f1773ba4f0c4aca993969
Matthew Cain <yooksi@zoho.com>
2020-04-23 23:28:48 +0200

    Add ModGenericException class

---
+++ src/main/java/io/yooksi/cococakes/lang/ModGenericException.java
@@ -0,0 +1,18 @@
+package io.yooksi.cococakes.lang;
+
+import io.yooksi.cococakes.CocoCakes;
+
+public class ModGenericException extends Exception {
+
+	public ModGenericException(String message) {
+		super(formatMessage(message));
+	}
+
+	public ModGenericException(String message, Throwable cause) {
+		super(formatMessage(message), cause);
+	}
+
+	private static String formatMessage(String message) {
+		return String.format("Exception occurred in %s mod: %s", CocoCakes.MODID, message);
+	}
+}
